1ecec3c04e1014dfeed00994cec37c88b222dd56
DeepakNaikofcl <deepaknaikofcl@gmail.com>
2024-12-24 15:48:14 +0530

    first commit

--- Demo.java
+++ Demo.java
@@ -1,10 +1,28 @@
 package com.spec.deepak;
 public class Demo {
-	int balance = 1000;
-	void deposit(int amt) {
+	
+	public static void main(String [] args) {
+		
+		int [] [] data = new int[2][3];
+		
+		data[0][0]=1;
+		data[0][1]=2;
+		data[0][2]=3;
+		
+		
+		data[1][0]=4;
+		data[1][1]=5;
+		data[1][2]=6;
+		
+		
+		for(int row = 0;row<2;row++) {
+			for(int col =0;col<3;col++) {
+				
+				System.out.print(data[row][col]+" ");
+			}
+			
+			System.out.println();
+		}
 		
 	}
-
-		
-	
 }
